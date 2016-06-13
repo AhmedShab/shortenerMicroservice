d522d602eca67094d37c49158f1cdb6290ec2347
AhmedShab <ahmed.vuw@gmail.com>
2016-06-14 00:09:26 +1200

    fixed my url pathname bug

--- routes/sites.js
+++ routes/sites.js
@@ -14,7 +14,11 @@ router.get('/', function(req, res) {
 */
 router.get('/new/:url*', function(req, res, next) {
   var checkUrl = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-  if (checkUrl.test(req.params.url)){
+
+  var original_url = req.originalUrl.slice(11);
+
+  console.log(original_url)
+  if (!checkUrl.test(original_url)){
     res.json({message: "you entered invalid url"})
   }
 
